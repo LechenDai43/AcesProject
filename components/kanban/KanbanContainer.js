ba71b4a38cf763909589619d78d756f766d73afe
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2021-03-13 14:11:01 -0500

    Skeleton of Kanban

---
+++ components/kanban/KanbanContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class KanbanContainer extends Component {
+    render(): React.ReactNode {
+        return (
+            <main>
+
+            </main>
+        );
+    }
+}
+
+export default KanbanContainer;
